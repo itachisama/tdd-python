95357710c5f14dcc99b1783c0c20e85a4cd6c36a
= <leandro.silva@uniriotec.br>
2013-09-11 19:51:19 +0000

    Fornecidas mensagens de erro nas funções de verificação

--- person.py
+++ person.py
@@ -2,12 +2,12 @@ class Person(object):
 
     def name_is_empty(self, name):
         if not name:
-            raise ValueError
+            raise ValueError('Name can\'t be empty!')
 
     def name_is_too_short(self, name):
         if len(name) < 4:
-            raise ValueError
+            raise ValueError('Name is too short!')
 
     def name_is_not_string(self, name):
         if not isinstance(name, basestring):
-            raise ValueError
+            raise ValueError('Invalid name!')
